bc33697e97a2ed9681aec90f13c034e4fe847d1d
tatecarson <tatecarson@gmail.com>
2019-03-13 17:10:25 -0500

    updated fade code

--- Hardware_Week_2_Th/fade/fade.ino
+++ Hardware_Week_2_Th/fade/fade.ino
@@ -1,37 +1,50 @@
+// PWM Pin
 int led = 9;
 int brightness = 200;
 int fadeAmount = 5;
-int fadeRate;
-// step 1. just turn the led on a given brightness
-// step 2. dim it over time
-// step 3. control it with a pot
+int fade;
 
-void setup()
-{
+void setup() {
   // because we're using a digital pin we have
   // to set the mode to input or output
-  pinMode(led, OUTPUT);
+//  pinMode(led, OUTPUT);
 
   Serial.begin(9600);
-  //
 }
 
-void loop()
-{
-  // set the brightness of pin 9
-  analogWrite(led, brightness);
-  fadeRate = analogRead(A0);
-  // increase brightness by fadeAmount
-  brightness = brightness + fadeAmount;
+void loop() {
+  int button1 = digitalRead(2);
+  int button2 = digitalRead(4);
+  
+  if (button1) {
+    // Control fade with pot
+    ///////////////////////
+    
+    // read from pot on A0
+    fade = analogRead(A0);
 
-  if (brightness <= 0 || brightness >= 255)
-  {
-    // toggle increase and decrease
-    fadeAmount = -fadeAmount;
+    // constrain the pot value 0 - 255
+    brightness = map(fade, 0, 1023, 0, 255);
+
+    // set the brightness of pin 9 with pot value
+    analogWrite(led, brightness);
+
+  } else if (button2) {
+    // autofade with button press
+    /////////////////////////////
+    
+    // increase brightness by fadeAmount
+    brightness = brightness + fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      // toggle increase and decrease
+      fadeAmount = -fadeAmount;
+    }
+    analogWrite(led, brightness);
+  } else {
+    // if neither button pressed turn LED off 
+    analogWrite(led, 0);
   }
 
-  // control the rate of fade with a pot
-  int fMap = map(fadeRate, 0, 1023, 10, 100);
-  Serial.println(fMap);
-  delay(fMap);
+  delay(30);
 }
