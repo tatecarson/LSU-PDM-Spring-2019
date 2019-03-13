c474f7340fc71507f583177c13158570171e3a9d
tatecarson <tatecarson@gmail.com>
2019-03-13 16:22:49 -0500

    added 2 more

--- Hardware_Week_2_Th/fade/fade.ino
+++ Hardware_Week_2_Th/fade/fade.ino
@@ -1,30 +1,33 @@
-int led = 9; 
-int brightness = 200; 
-int fadeAmount = 5; 
-int fadeRate; 
+int led = 9;
+int brightness = 200;
+int fadeAmount = 5;
+int fadeRate;
 // step 1. just turn the led on a given brightness
 // step 2. dim it over time
-// step 3. control it with a pot 
+// step 3. control it with a pot
 
-void setup() {
-  // because we're using a digital pin we have 
+void setup()
+{
+  // because we're using a digital pin we have
   // to set the mode to input or output
-  pinMode(led, OUTPUT); 
-  
-  Serial.begin(9600);
+  pinMode(led, OUTPUT);
 
+  Serial.begin(9600);
+  //
 }
 
-void loop() {
-  // set the brightness of pin 9 
+void loop()
+{
+  // set the brightness of pin 9
   analogWrite(led, brightness);
   fadeRate = analogRead(A0);
   // increase brightness by fadeAmount
   brightness = brightness + fadeAmount;
-  
-  if(brightness <= 0 || brightness >= 255){
+
+  if (brightness <= 0 || brightness >= 255)
+  {
     // toggle increase and decrease
-    fadeAmount =- fadeAmount;
+    fadeAmount = -fadeAmount;
   }
 
   // control the rate of fade with a pot
