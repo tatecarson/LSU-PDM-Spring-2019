ff5f0d5cbd3fd5323602e4ddf9d4b9dd26ad4d28
tatecarson <tatecarson@gmail.com>
2019-03-21 11:18:58 -0500

    some updates

--- Hardware_Week_2_Th/fade_StateChangeDetection/fade_StateChangeDetection.ino
+++ Hardware_Week_2_Th/fade_StateChangeDetection/fade_StateChangeDetection.ino
@@ -14,9 +14,9 @@ int button1 = 0;
 int button2 = 0;
 int button1State;
 int lastButton1State = LOW;
-int button2State;
-int lastButton2State = LOW;
-int buttonPushCounter = 0; 
+int button2;
+int lastButton2 = LOW;
+int buttonPushCounter = 0;
 
 // states of the program
 int fadeState = HIGH;
@@ -24,11 +24,12 @@ int onState = HIGH;
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 unsigned long lastDebounceTime2 = 0;
 
-void setup() {
+void setup()
+{
   // because we're using a digital pin we have
   // to set the mode to input or output
   pinMode(led, OUTPUT);
@@ -37,49 +38,58 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // toggle between fading and using pot
   button1 = digitalRead(2);
   Serial.println(button1);
   // If the switch changed, due to noise or pressing:
-  if (button1 != lastButton1State) {
+  if (button1 != lastButton1State)
+  {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (button1 != button1State) {
+    if (button1 != button1State)
+    {
       button1State = button1;
 
       // only toggle the LED if the new button state is HIGH
-      if (button1State) {
+      if (button1State)
+      {
         fadeState = !fadeState;
-        Serial.println(fadeState); 
+        Serial.println(fadeState);
       }
     }
   }
-  
 
   // save the reading. Next time through the loop, it'll be the lastButtonState:
   lastButton1State = button1;
 
   // this button will turn off LED after its pressed 4 times
-  button2State = digitalRead(4);
+  // we're not using the whole debouncing function here
+  button2 = digitalRead(4);
 
-  if (button2State != lastButton2State) {
+  if (button2 != lastButton2)
+  {
     // if the state has changed, increment the counter
-    if (button2State == HIGH) {
+    if (button2 == HIGH)
+    {
       // if the current state is HIGH then the button went from off to on:
       buttonPushCounter++;
       Serial.println("on");
       Serial.print("number of button pushes: ");
       Serial.println(buttonPushCounter % 4);
       Serial.println(onState);
-    } else {
+    }
+    else
+    {
       // if the current state is LOW then the button went from on to off:
       Serial.println("off");
     }
@@ -87,46 +97,53 @@ void loop() {
     delay(50);
   }
   // save the current state as the last state, for next time through the loop
-  lastButton2State = button2State;
+  lastButton2 = button2;
 
   // change onState
-  if(buttonPushCounter % 4 == 0) {
+  if (buttonPushCounter % 4 == 0)
+  {
     onState = true;
-  } else {
+  }
+  else
+  {
     onState = false;
   }
 
-  //  Serial.println(button2State);
+  //  Serial.println(button2);
 
-  if (fadeState && onState) {
+  if (fadeState && onState)
+  {
     // Control fade with pot
     ///////////////////////
 
     // read from pot on A0
     pot = analogRead(A0);
-  
+
     // constrain the pot value 0 - 255
     brightness = map(pot, 0, 1023, 0, 255);
 
     // set the brightness of pin 9 with pot value
     analogWrite(led, brightness);
-
-  } else if (!fadeState && onState) {
+  }
+  else if (!fadeState && onState)
+  {
     // autofade with button press
     /////////////////////////////
 
     // increase brightness by fadeAmount
     brightness = brightness + fadeAmount;
 
-    if (brightness <= 0 || brightness >= 255) {
+    if (brightness <= 0 || brightness >= 255)
+    {
       // toggle increase and decrease
       fadeAmount = -fadeAmount;
     }
     analogWrite(led, brightness);
-  } else if(!onState) {
+  }
+  else if (!onState)
+  {
     digitalWrite(led, LOW);
   }
-
 
   delay(30);
 }
--- Hardware_Week_2_Th/fade_addDebounce/fade_addDebounce.ino
+++ Hardware_Week_2_Th/fade_addDebounce/fade_addDebounce.ino
@@ -23,11 +23,12 @@ int onState = HIGH;
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 unsigned long lastDebounceTime2 = 0;
 
-void setup() {
+void setup()
+{
   // because we're using a digital pin we have
   // to set the mode to input or output
   pinMode(led, OUTPUT);
@@ -36,27 +37,36 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // toggle between fading and using pot
+  // fadeState
   button1 = digitalRead(2);
 
-  // If the switch changed, due to noise or pressing:
-  if (button1 != lastButton1State) {
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  if (button1 != lastButton1State)
+  {
     // reset the debouncing timer
     lastDebounceTime = millis();
     // Serial.println(lastDebounceTime);
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (button1 != button1State) {
+    if (button1 != button1State)
+    {
       button1State = button1;
 
       // only toggle the LED if the new button state is HIGH
-      if (button1State) {
+      if (button1State)
+      {
         fadeState = !fadeState;
       }
     }
@@ -67,27 +77,31 @@ void loop() {
 
   Serial.println(onState);
 
-
   // turn everything on or off
+  // changes onState to true or false
   button2 = digitalRead(4);
 
   // If the switch changed, due to noise or pressing:
-  if (button2 != lastButton2State) {
+  if (button2 != lastButton2State)
+  {
     // reset the debouncing timer
     lastDebounceTime2 = millis();
     // Serial.println(lastDebounceTime);
   }
 
-  if ((millis() - lastDebounceTime2) > debounceDelay) {
+  if ((millis() - lastDebounceTime2) > debounceDelay)
+  {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (button2 != button2State) {
+    if (button2 != button2State)
+    {
       button2State = button2;
 
       // only toggle the LED if the new button state is HIGH
-      if (button2State) {
+      if (button2State)
+      {
         onState = !onState;
       }
     }
@@ -96,10 +110,10 @@ void loop() {
   // save the reading. Next time through the loop, it'll be the lastButtonState:
   lastButton2State = button2;
 
-
   //  Serial.println(button2State);
 
-  if (fadeState && onState) {
+  if (fadeState && onState)
+  {
     // Control fade with pot
     ///////////////////////
 
@@ -111,23 +125,26 @@ void loop() {
 
     // set the brightness of pin 9 with pot value
     analogWrite(led, brightness);
-
-  } else if (!fadeState && onState) {
+  }
+  else if (!fadeState && onState)
+  {
     // autofade with button press
     /////////////////////////////
 
     // increase brightness by fadeAmount
     brightness = brightness + fadeAmount;
 
-    if (brightness <= 0 || brightness >= 255) {
+    if (brightness <= 0 || brightness >= 255)
+    {
       // toggle increase and decrease
       fadeAmount = -fadeAmount;
     }
     analogWrite(led, brightness);
-  } else if(!onState) {
+  }
+  else if (!onState)
+  {
     digitalWrite(led, LOW);
   }
-
 
   delay(30);
 }
