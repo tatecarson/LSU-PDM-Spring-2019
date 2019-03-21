9f1f978ec8d2fdf42b8b3bb338bdc0ffdbef03da
tatecarson <tatecarson@gmail.com>
2019-03-21 12:19:26 -0500

    update tone melody

--- Hardware_Week_2_Th/toneMelody_theramin/toneMelody_theramin.ino
+++ Hardware_Week_2_Th/toneMelody_theramin/toneMelody_theramin.ino
@@ -13,24 +13,26 @@
   This example code is in the public domain.
 
   http://www.arduino.cc/en/Tutorial/Tone
+
+  https://www.arduino.cc/en/uploads/Tutorial/Tone_Fritzing.png
 */
 
 #include "pitches.h"
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
+    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
-};
+    4, 8, 8, 4, 4, 4, 4, 4};
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  for (int thisNote = 0; thisNote < 8; thisNote++)
+  {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
@@ -46,10 +48,10 @@ void setup() {
   }
 }
 
-void loop() {
+void loop()
+{
   // theramin
   int photo = analogRead(A0);
-  
 
   // test for the room you're in
   int freq = map(photo, 150, 700, 100, 1000);
@@ -58,6 +60,6 @@ void loop() {
   tone(8, freq, 10);
 
   // now add a button to turn this on and off
-  
+
   delay(1);
 }
